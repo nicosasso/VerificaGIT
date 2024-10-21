a7eb81b4a85fb1fa592be51b9501377ff8c3bba7
Nicholas Sassano <NICHOLAS.SASSANO@local.ittcesena.it>
2024-10-21 08:45:40 +0200

    Release 0.2.0: Inserita la classe Biblioteca

---
+++ VerificaGIT/Biblioteca.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGIT
+{
+    internal class Biblioteca
+    {
+        private string nome;
+        private string indirizzo;
+        private string orario_apertura;
+        private string orario_chiusura;
+        private List<Libro> libri;
+
+        public Biblioteca(string nome, string indirizzo, string orario_apertura, string orario_chiusura, List<Libro> libri)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orario_apertura = orario_apertura;
+            this.orario_chiusura = orario_chiusura;
+            this.libri = libri;
+        }
+
+        public void AggiungiLibro(Libro libro)
+        {
+            libri.Add(libro);
+        }
+
+        public bool CercaLibroConTitolo(string titolo)
+        {
+            for (int i = 0; i < libri.Count; i++)
+            {
+                if (libri[i].Titolo.ToUpper() == titolo.ToUpper())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Libro> CercaLibriConAutore(string autore)
+        {
+            List<Libro> libri_stesso_autore = new List<Libro>();
+
+            for (int i = 0; i < libri.Count; i++)
+            {
+                if (libri[i].Autore.ToUpper() == autore.ToUpper())
+                {
+                    libri_stesso_autore.Add(libri[i]);
+                }
+            }
+
+            return libri_stesso_autore;
+        }
+
+        public int LibriPresenti()
+        {
+            return libri.Count;
+        }
+            
+    }
+}
