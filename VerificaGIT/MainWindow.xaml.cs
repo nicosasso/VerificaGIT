680b9b0ea777f3668f5845389759ecdfc4767a57
Nicholas Sassano <NICHOLAS.SASSANO@local.ittcesena.it>
2024-10-21 09:41:32 +0200

    Release 1.0.0: Progetto concluso

--- VerificaGIT/MainWindow.xaml.cs
+++ VerificaGIT/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,36 +17,52 @@ namespace VerificaGIT
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Biblioteca biblioteca;
+        Libro libro;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            libro = new Libro("", "", "", "", -1);
+            List<Libro> libri = new List<Libro>();
+            biblioteca = new Biblioteca("", "", "", "", libri);
         }
 
         private void Aggiungi_Click(object sender, RoutedEventArgs e)
         {
             if (Autore.Text == "" || Titolo.Text == "" || AnnoPubblicazione.Text == "" || Editore.Text == "" || Pagine.Text == "")
                 MessageBox.Show("Ci sono dei campi vuoti, riempili"); 
-            //else
-            //{
-            //    Libro libro = new Libro(Autore.Text, Titolo.Text, AnnoPubblicazione.Text, Editore.Text, Pagine.Text);
-                
-            //}
+            else
+            {
+                Libro libro = new Libro(Autore.Text, Titolo.Text, AnnoPubblicazione.Text, Editore.Text, int.Parse(Pagine.Text));
+                biblioteca.AggiungiLibro(libro);
+                MessageBox.Show("Libro aggiunto correttamente");
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (TitoloTrovare.Text == "")
                 MessageBox.Show("Devi inserire un titolo valido. riprova");
-            //else
-            //{
-            //    TODO: Richiamare la funzione corretta
+            else
+            {
+                if(biblioteca.CercaLibroConTitolo(TitoloTrovare.Text) == true)
+                {
+                    MessageBox.Show("Il libro è presente nella biblioteca");
+                }
+                else
+                {
+                    MessageBox.Show("Il libro non è presente nella biblioteca");
+                }
 
-            //}
+            }
         }
 
         private void Calcola_Click(object sender, RoutedEventArgs e)
         {
-            // Numero.Text = TODO
+            Numero.Text = $"{biblioteca.LibriPresenti()}";
         }
 
         private void Lettura_CLick(object sender, RoutedEventArgs e)
@@ -54,8 +71,13 @@ namespace VerificaGIT
                 MessageBox.Show("Ci sono dei campi vuoti, riempili");
             else
             {
-                //TODO: richiamare funzione corretta
+                MessageBox.Show($"Tempo di lettura = {libro.ReadingTime}");
             }
+        }
+
+        private void String_CLick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Autore = {Autore.Text}, Titolo = {Titolo.Text}, Anno Pubblicazione = {AnnoPubblicazione.Text}, Editore = {Editore.Text}, Pagine = {Pagine.Text}");
         }
     }
 }
